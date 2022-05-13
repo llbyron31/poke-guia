4f7b49d0d34869ad18eb160df4109fdc4d5f0cab
llbyron31 <82618775+llbyron31@users.noreply.github.com>
2022-05-13 00:30:39 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img class="pokemon--image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" alt="">
+    <h1>PokeGuía</h1>
+    <poke-guia></poke-guia>
+  </div>
+</template>
+
+<script>
+import PokeGuia from './components/PokeGuia.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PokeGuia
+  }
+}
+</script>
+
+<style>
+
+*{
+  text-align: center;
+  font-family: 'Press Start 2P', cursive;
+}
+p{
+  margin: 0;
+}
+.pokemon--image{
+  width: 400px;
+  margin: 40px 0;
+}
+h1{
+  margin: 40px 0;
+}
+</style>
